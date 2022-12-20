43dea88f848ac0e31bc4fb371898e762477d3d71
kashifaxioned <kashifa@axioned>
2022-12-20 18:50:34 +0530

    fix: eslint error resolved in app.jsx file

--- pages/_app.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable react/jsx-props-no-spreading */
-import React from "react";
-import "../styles/globals.css";
-
-// eslint-disable-next-line react/prop-types
-export default function App({ Component, pageProps }) {
-  // eslint-disable-next-line react/jsx-filename-extension
-  return <Component {...pageProps} />;
-}
